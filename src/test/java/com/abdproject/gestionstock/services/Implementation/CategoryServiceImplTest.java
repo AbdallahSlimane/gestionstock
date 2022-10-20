857da2f498e4ed05997da8a26366414f3cb48a30
AbdallahSlimane <slimane.abdallah75@gmail.com>
2022-10-20 09:03:07 +0200

    ci-cd-01

--- src/test/java/com/abdproject/gestionstock/services/Implementation/CategoryServiceImplTest.java
+++ src/test/java/com/abdproject/gestionstock/services/Implementation/CategoryServiceImplTest.java
@@ -3,14 +3,16 @@ package com.abdproject.gestionstock.services.Implementation;
 import com.abdproject.gestionstock.dto.CategoryDto;
 import com.abdproject.gestionstock.services.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CategoryServiceImplTest {
 
